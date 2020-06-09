697aba3c34f313f48c5ba69f5cb77be90f81f93d
suejohnmo <susan.mwaura@gmail.com>
2020-06-09 13:19:41 +0300

    adding new files to the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
-$("h1").click(function () {
-  alert("This is a heading tag");
-});
+// $("h1").click(function () {
+//   alert("This is a heading tag");
+// });
 
-$("p").click(function () {
-  alert("This is a paragraph");
-});
+// $("p").click(function () {
+//   alert("This is a paragraph");
+// });
 
-$("img").click(function () {
-  alert("This is an image");
+// $("img").click(function () {
+//   alert("This is an image");
+// });
+// $("h1").click(function () {
+//   alert("This is a header.");
+//   alert("I told you, THIS IS A HEADER!");
+// });
+$(document).ready(function () {
+  $("p").click(function () {
+    $("img").show();
+  });
 });
-$("h1").click(function () {
-  alert("This is a header.");
-  alert("I told you, THIS IS A HEADER!");
+$(document).ready(function () {
+  $("p").click(function () {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
 });
