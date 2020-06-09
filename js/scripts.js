4f3d003c825ad3c8ed2647deedd408b95e05e7e8
suejohnmo <susan.mwaura@gmail.com>
2020-06-09 12:35:55 +0300

    adding new files to the project

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$("h1").click(function () {
+  alert("This is a heading tag");
+});
+
+$("p").click(function () {
+  alert("This is a paragraph");
+});
+
+$("img").click(function () {
+  alert("This is an image");
+});
+$("h1").click(function () {
+  alert("This is a header.");
+  alert("I told you, THIS IS A HEADER!");
+});
